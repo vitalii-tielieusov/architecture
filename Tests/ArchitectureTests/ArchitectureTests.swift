2223ac370303aacbe9ce6abe75ce4b81c241b4ca
Vitaliy Teleusov <Vitaliy82tvn@gmail.com>
2022-04-25 01:40:45 +0300

    Refactoring

--- Tests/ArchitectureTests/ArchitectureTests.swift
+++ Tests/ArchitectureTests/ArchitectureTests.swift
@@ -6,6 +6,6 @@ final class ArchitectureTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Architecture().text, "Hello, World!")
+//        XCTAssertEqual(Architecture().text, "Hello, World!")
     }
 }
